ac96acaee3a325df427930d8d8f26320eeb2f746
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-11-17 23:50:48 -0300

    feat: add unform to watch the form :ok_hand:

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -8,35 +8,37 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
 
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-      <form>
-        <h1>Faça seu cadastro</h1>
+        <form>
+          <h1>Faça seu cadastro</h1>
 
-        <Input icon={FiUser} name="name" placeholder="Nome" />
-        <Input icon={FiMail} name="email" placeholder="E-mail" />
+          <Input icon={FiUser} name="name" placeholder="Nome" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
 
-        <Input
-          icon={FiLock}
-          name="password"
-          type="password"
-          placeholder="Senha"
-        />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Senha"
+          />
 
-        <Button type="submit">Cadastrar</Button>
-      </form>
+          <Button type="submit">Cadastrar</Button>
+        </form>
 
-      <a href="login">
-        <FiArrowLeft />
-        Voltar para logon
-      </a>
-    </Content>
-  </Container>
-);
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
