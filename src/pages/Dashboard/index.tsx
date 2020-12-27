aa62f696e9ade05ed097f1f6b72e7a0b26059c8c
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-12-26 23:23:22 -0300

    feat: :ok_hand: next appointment content

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointments,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -28,6 +37,31 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>dia</span>
+            <span>tlatlal</span>
+          </p>
+
+          <NextAppointments>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src="" alt="" />
+
+              <strong>VInicius</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointments>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
