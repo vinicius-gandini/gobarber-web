37f16e70680de28c0d169fc83957e4df39c72c05
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2021-01-06 21:25:37 -0300

    fix: :wrench: day not changing when click, fixed

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -49,7 +49,7 @@ const Dashboard: React.FC = () => {
   const [appointments, setAppointments] = useState<Appointment[]>([]);
 
   const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
-    if (modifiers.available && modifiers.disabled) {
+    if (modifiers.available && !modifiers.disabled) {
       setSelectedDate(day);
     }
   }, []);
